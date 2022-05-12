a62320f4b3e6139a64d33321d8133b78070593b6
pablo527 <pablo_7v.m@hotmail.com>
2022-05-13 01:59:50 +0200

    base structure of system is added

---
+++ file_Manager/src/main/java/com/example/file/FileManaegerApplication.java
@@ -0,0 +1,13 @@
+package com.example.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileManaegerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileManaegerApplication.class, args);
+	}
+
+}
---
+++ kernel/src/test/java/com/example/kernel/KernelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KernelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
